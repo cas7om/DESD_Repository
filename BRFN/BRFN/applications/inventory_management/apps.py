4483e5e2469768e8482fe77458bb29d841f27441
l8-quick <l8-quick@live.uwe.ac.uk>
2026-02-26 16:36:04 +0000

    #14 - introducing inventory management app - initially for allergens but to be expanded for products

---
+++ BRFN/BRFN/applications/inventory_management/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class inventory_managementConfig(AppConfig):
+    name = 'applications.inventory_management'
--- BRFN/BRFN/config/settings.py
+++ BRFN/BRFN/config/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "applications.account_management",
     "applications.order_management",
+    "applications.inventory_management",
 ]
 
 MIDDLEWARE = [
