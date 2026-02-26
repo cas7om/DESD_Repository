7a74d234698f7df0d74a21a0a84bfbee2882c37a
l8-quick <l8-quick@live.uwe.ac.uk>
2026-02-26 13:02:16 +0000

    User role in session for dynamic page views

--- BRFN/BRFN/Applications/AccountManagement/context_processors.py
+++ BRFN/BRFN/Applications/AccountManagement/context_processors.py
@@ -7,7 +7,17 @@ def current_user(request):
     user = None
     if user_id:
         user = User.objects.filter(id=user_id).first()
+        return {
+            "current_user": user,
+            "is_logged_in": user is not None,
+            "is_admin": user.has_role("Admin"),
+            "is_producer": user.has_role("Producer"),
+            "is_customer": user.has_role("Customer"),
+        }
     return {
         "current_user": user,
         "is_logged_in": user is not None,
+        "is_admin": False,
+        "is_producer": False,
+        "is_customer": False,
     }
